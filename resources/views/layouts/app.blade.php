e6fc90a62b2b3eff4e7721240d92cdd4fe7f869b
Jeremy Patterson <jrmypttrsn@gmail.com>
2018-10-17 20:38:55 -0500

    generate tailwind config, add nunito font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
+      <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>{{ config('app.name', 'OneCert OID') }}</title>
   </head>
-  <body class="font-sans antialiased text-black leading-tight">
+  <body class="font-sans antialiased text-grey-darkest leading-tight">
     <div id="app">
       @yield('body')
     </div>
--- tailwind.js
+++ tailwind.js
@@ -206,6 +206,7 @@ module.exports = {
       'Droid Sans',
       'Helvetica Neue',
       'sans-serif',
+      'Nunito',
     ],
     'serif': [
       'Constantia',
